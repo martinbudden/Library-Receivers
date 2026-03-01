49c1a7d768b4f69c57dec29f4feadd76912d03d1
Martin Budden <mjbudden@gmail.com>
2026-03-01 13:57:34 +0000

    Renamed pg to ctx.

--- src/cockpit_base.h
+++ src/cockpit_base.h
@@ -22,8 +22,8 @@ public:
     uint32_t get_timeout_ticks() const { return _timeout_ticks; }
     void set_timeout_ticks(uint32_t timeout_ticks) { _timeout_ticks = timeout_ticks; }
 
-    virtual void update_controls(uint32_t tick_count, const ReceiverBase& receiver, receiver_context_t& pg) = 0;
-    virtual void check_failsafe(uint32_t tick_count, receiver_context_t& pg) = 0;
+    virtual void update_controls(uint32_t tick_count, const ReceiverBase& receiver, receiver_context_t& ctx) = 0;
+    virtual void check_failsafe(uint32_t tick_count, receiver_context_t& ctx) = 0;
 protected:
     uint32_t _timeout_ticks {100};
 };
